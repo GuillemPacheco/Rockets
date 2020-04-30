f942535010ef513b6a01764ae00494a394b76149
Guillem <gpacheco@edu.tecnocampus.cat>
2020-04-30 13:52:06 +0200

    AbsCircuit

--- src/DomainCircuit/AbsCircuit.java
+++ src/DomainCircuit/AbsCircuit.java
@@ -8,7 +8,28 @@ public abstract class AbsCircuit {
 	
 	public AbsCircuit(String circuitName, float distance, double time) throws Exception {
 		if(circuitName == null || circuitName.isEmpty()) {
-			
+			throw new Exception("The name is not correct");
 		}
+		if(distance<=0) {
+			throw new Exception("The distance cannot under or equal 0");
+		}
+		if(time<=0) {
+			throw new Exception("The time cannot under or equal 0");
+		}
+		this.circuitName=circuitName;
+		this.distance=distance;
+		this.time=time;
+	}
+	
+	public float getDistance() {
+		return distance;
+	}
+	
+	public double getTime() {
+		return time;
+	}
+	
+	public String getCircuitName() {
+		return circuitName;
 	}
 }
