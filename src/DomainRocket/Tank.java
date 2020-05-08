bddf7fc170ac63aba433491111a9ab3a372ec5af
martinpiqueras <martinpiqueras@192.168.1.8>
2020-05-08 12:08:16 +0200

    Classes updated

--- src/DomainRocket/Tank.java
+++ src/DomainRocket/Tank.java
@@ -2,11 +2,22 @@ package DomainRocket;
 
 public class Tank {
 	protected float capacity;
+	protected float currentGasoline;
 	public Tank (float capacity) {
 		this.capacity = capacity;
+		currentGasoline = capacity; // Starts full;
 	}
 	
 	public float getCapacity () {
 		return capacity;
+	}
+	
+	public float getCurrentGasoline () {
+		return currentGasoline;
+	}
+	
+	public void updateCurrentGasoline (float newGasolineValue) {
+		
+		currentGasoline = newGasolineValue;
 	}
 }
