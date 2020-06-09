65f44c244500c3bf3656a3b704fd45f4ef863141
Guillem <gpacheco@edu.tecnocampus.cat>
2020-06-09 21:06:50 +0200

    Little change main

--- src/view/Main.java
+++ src/view/Main.java
@@ -15,7 +15,6 @@ public class Main {
 		public static void main(String[] args) throws Exception {	
 			Controller controller=new Controller();
 			controller.createCircuit();
-			System.out.println(controller.getCircuit().getCircuitName());
 			startCompetition(controller.getCircuit());
 		}
 
