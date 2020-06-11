f78a6189be946a36b9dfe1ffb178cb897f34a106
martinpiqueras <martinpiqueras@192.168.1.8>
2020-06-11 14:47:54 +0200

    Commit

--- src/view/Main.java
+++ src/view/Main.java
@@ -5,15 +5,12 @@ import application.Controller;
 
 
 public class Main {
-	
 		
 		public static void main(String[] args) throws Exception {	
+			System.out.println("Initializing Rockets... This may take a while");
 			Controller controller=new Controller();
 			controller.createCircuit();
 			System.out.println(controller.startCompetition());
 		}
-
-
-		
 
 }
